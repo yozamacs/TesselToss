940fdfb65b85cc8a1e86f785dedf60533cf44492
yozamacs <yozamacs@yahoo.com>
2014-10-26 13:29:22 -0700

    reformat graphy.py and make channel name anonymous

--- graphy.py
+++ graphy.py
@@ -1,14 +1,14 @@
 import pygal
-
+import time
 from Pubnub import Pubnub
+
 pubnub = Pubnub(publish_key="pub-c-YOUR-PUBNUB-PUBLISH-KEY-HERE", subscribe_key="sub-c-YOUR-PUBNUB-SUBSCRIBE-KEY-HERE", ssl_on=False)
-channel = 'lollipop'
-graphData = []
+channel = 'YOUR-CHANNEL-NAME-HERE'
+rawData = []
 
 def callback(message, channel):
-    print(message)
     for item in message:
-    	graphData.append(item)
+        rawData.append(item)
 
 def error(message):
     print("ERROR : " + str(message))
@@ -27,25 +27,31 @@ def disconnect(message):
 pubnub.subscribe(channel, callback=callback, error=callback,
                  connect=connect, reconnect=reconnect, disconnect=disconnect)  
 
-import time
-time.sleep(15)
+
+dataCollectionInterval = 15
+time.sleep(dataCollectionInterval)
 pubnub.unsubscribe(channel=channel)
-chartData = []
-chartDataY = []
-print "graph data:"
-print graphData
+
+print "Raw data:"
+print rawData
+
+
 time=0
-for item in graphData:
-	chartData.append((time,item[0]))
-	chartDataY.append((time,item[1]))
-	time+=1
+chartDataX = []
+chartDataY = []
+
+for item in rawData:
+    chartDataX.append((time,item[0]))
+    chartDataY.append((time,item[1]))
+    time+=1
 
 print "chart data: "
-print chartData
-print chartDataY                                           
+print chartDataX
+print chartDataY  
+
 xy_chart = pygal.XY(stroke=True)
 xy_chart.title = 'X and Y vs Time'                                        
-xy_chart.add('X', chartData) 
+xy_chart.add('X', chartDataX) 
 xy_chart.add('Y', chartDataY)
 xy_chart.render_to_file('XY_chart.svg')  
 print "I'm done!" 
